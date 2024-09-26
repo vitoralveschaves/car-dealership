3c071e6aa944b8713d1bed75129fed9bd7b4e6d7
vitoralveschaves <vitorchavesalv@gmail.com>
2024-09-26 17:16:49 -0300

    feat: setup project

---
+++ src/main/java/br/com/api/dealership/CarDealershipApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.dealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/dealership/CarDealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.dealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
