3b46197c9ae1a74aa185a511214ce588bae48096
vitoralveschaves <vitorchavesalv@gmail.com>
2024-09-27 15:56:46 -0300

    feat: add CarRepository interface

---
+++ src/main/java/br/com/api/dealership/repositories/CarRepository.java
@@ -0,0 +1,11 @@
+package br.com.api.dealership.repositories;
+
+import br.com.api.dealership.entities.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, UUID> {
+}
