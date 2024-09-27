7c314f90295fddec3756fcbc6664d2f3c9ef707a
vitoralveschaves <vitorchavesalv@gmail.com>
2024-09-27 16:05:44 -0300

    feat: add MakeController class

---
+++ src/main/java/br/com/api/dealership/controllers/MakeController.java
@@ -0,0 +1,33 @@
+package br.com.api.dealership.controllers;
+
+import br.com.api.dealership.dtos.MakeRequestDto;
+import br.com.api.dealership.dtos.MakeResponseDto;
+import br.com.api.dealership.services.MakeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/make")
+public class MakeController {
+
+    private final MakeService makeService;
+
+    public MakeController(MakeService makeService) {
+        this.makeService = makeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MakeResponseDto> save(@RequestBody MakeRequestDto request) {
+        var savedMake = makeService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedMake);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MakeResponseDto>> getAll() {
+        var listMake = makeService.getAll();
+        return ResponseEntity.status(HttpStatus.OK).body(listMake);
+    }
+}
